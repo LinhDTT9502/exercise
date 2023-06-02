96ca482763cda0ddb29be7691d45c9a03acad278
truc linh <duongthitruclinh257@gmail.com>
2023-06-03 02:15:52 +0700

    exercise56789

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class Footer extends Component{
+    render(){
+        return(
+            <div>
+                <a className="footerText">copyright © 2023</a>
+            </div>
+        )
+    }
+}
